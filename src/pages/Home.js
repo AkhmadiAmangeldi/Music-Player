47c7b9be3e1101d34083aa875c9833643df54161
Dimash Amalbek <d_amalbek@kbtu.kz>
2024-10-16 17:44:55 +0500

    design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <h2>Добро пожаловать в unTaspa</h2>
       <p>Откройте для себя популярные плейлисты и подкасты.</p>
     </div>
